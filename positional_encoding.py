55fbdb3724030f52c2e6eadccd0706021628f41a
mhdnihas <muhammednihas8589@gmail.com>
2024-11-10 20:47:54 +0530

    Multihead attention

--- positional_encoding.py
+++ positional_encoding.py
@@ -8,8 +8,7 @@ def positional_encoding(pos,d_model=512):
         pe[0][i]=math.sin(pos/10000**(2*i/d_model))
         pe[0][i+1]=math.cos(pos/10000**(2*i/d_model))
 
-
-
     print(pe)
+    return pe
 
 positional_encoding(1)
